0609ba0a93c1a002aff1c99faac24197e6ab83e4
Dylan <dylan_krajnc@tutanota.com>
2023-08-08 19:00:29 +0200

    - Completed the lesson portion of section 2

---
+++ OOP-CSharp/Characters/Warriors/Warrior.cs
@@ -0,0 +1,27 @@
+﻿namespace Characters.Warriors
+{
+    public class Warrior
+    {
+        // fields
+        private int height;
+        private int weight;
+        private string name;
+
+        // Properties
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public Warrior(int height, int weight, string name)
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+
+        public void Greet(Warrior warrior)
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");
+        }
+    }
+}
---
+++ OOP-CSharp/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Characters.Warriors;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        Warrior theGoodGuy = new Warrior(180, 90, "Good Guy");
+        Warrior theBadGuy = new Warrior(170, 70, "Bad Guy");
+
+        theGoodGuy.Greet(theBadGuy);
+        theBadGuy.Greet(theGoodGuy);
+    }
+}
